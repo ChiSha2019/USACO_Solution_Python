f428b8a99bd545d292157cd4a629db954ef489fc
Qi Xia <qixia@Qis-iMac.local>
2020-11-07 14:59:38 -0600

    social distance finished

--- Bronze/raceJan2020/race_class_demo.py
+++ Bronze/raceJan2020/race_class_demo.py
@@ -1,6 +1,37 @@
 '''
+given distance to find min time <=> given time find max distance
+
 we acceclerate as much as possible then decrease the speed to meet Vfinal
 1) the distsance is not great enough or vfinal is not small enough of speed decrement, which we just need
 to accelerate
+d = (1+Vf)/2*Vf
+t = (sqrt(1+8d)-1)/2a
 2) you need to increase then drop your speed
+d = t^2/4 + Vf/2*t - Vf^2/4+Vf/2 =>
+t = -Vf + sqrt(2Vf^2 -2 Vf + 4d)
 '''
+import math
+
+with open("race.in", "r") as input_file:
+    line1 = input_file.readline().split()
+    total_dist = int(line1[0])
+    num_speed = int(line1[1])
+    v_final = []
+    for i in range(0, num_speed):
+        v_final.append(int(input_file.readline()))
+
+    with open("race.out", "w") as output_file:
+        for vf_item in v_final:
+            time = -1
+            dist_accelerate_to_vf = (1 + vf_item) * vf_item / 2
+            if dist_accelerate_to_vf >= total_dist:
+                time = math.ceil((math.sqrt(1 + 8 * total_dist) - 1) / 2)
+            else:
+                time_sharptop_decimal = math.sqrt(2 * vf_item * vf_item + 4 * total_dist - 2 * vf_item) - vf_item
+                # if use sharptop has to use time_sharptop to finish such distance
+                time = math.ceil(time_sharptop_decimal)
+            '''
+            check if we need to switch to flat top
+            '''
+            output_file.write(str(time) + "\n")
+
--- Bronze/social_distancing1/Trytry.py
+++ Bronze/social_distancing1/Trytry.py
@@ -1,13 +1,11 @@
-#"""Returns size of largest gap between two 1s and also the index where it starts"""
 gap_start = -1
 
 
-def copy_list(old_list):
+def copy_list(old_list: list) -> list:
     new_list = []
     for item in old_list:
         new_list.append(item)
     return new_list
-
 
 def find_largest_interior_gap(stall_list):
     biggest_gap = 0
@@ -25,7 +23,6 @@ def find_largest_interior_gap(stall_list):
 
     return biggest_gap
 
-
 #'''Returns size of smallest gap between two 1s'''
 
 
@@ -40,7 +37,6 @@ def find_smallest_interior_gap(stall_list):
             current_start = i
     return smallest_gap
 
-
 def try_cow_in_largest_gap(stall_list):
     global gap_start
     largest_gap = find_largest_interior_gap(stall_list)
@@ -50,7 +46,6 @@ def try_cow_in_largest_gap(stall_list):
         return find_smallest_interior_gap(stall_list)
     return -1  # no gap
 
-
 with open("socdist1.in", "r") as input_file:
     stall_num = int(input_file.readline())
     current_stall = []
@@ -59,38 +54,12 @@ with open("socdist1.in", "r") as input_file:
         current_stall.append(input_file.read(1))
 
     answer = 0
-    #'''Possibility 1. put two cows in largest interior gap'''
+    # '''Possibility 1. put two cows in largest interior gap'''
     largest_gap = find_largest_interior_gap(current_stall)
     if largest_gap >= 3:
         temp = copy_list(current_stall)
-        temp[int(gap_start + largest_gap / 3)] = "1"
-        temp[int(gap_start + largest_gap * 2 / 3)] = "1"
-        answer = max(answer, find_smallest_interior_gap(temp))
-
-    #'''Possibility 2. cows at both ends'''
-    if current_stall[0] == "0" and current_stall[stall_num - 1] == "0":
-        temp = copy_list(current_stall)
-        temp[0] = "1"
-        temp[stall_num - 1] = "1"
-        answer = max(answer, find_smallest_interior_gap(temp))
-
-    #'''Possibility 3. cow at left + cow in largest interior gap'''
-    if current_stall[0] == "0":
-        temp = copy_list(current_stall)
-        temp[0] = "1"
-        answer = max(answer, try_cow_in_largest_gap(temp))
-
-    #'''Possibility 4. cow at right + cow in largest interior gap'''
-    if current_stall[stall_num - 1] == "0":
-        temp = copy_list(current_stall)
-        temp[stall_num - 1] = "1"
-        answer = max(answer, try_cow_in_largest_gap(temp))
-
-    #'''Possibility 5. cow at largest interior gap.  done twice'''
-    if largest_gap >= 2:
-        temp = copy_list(current_stall)
-        temp[int(gap_start + largest_gap / 2)] = "1"
-        answer = max(answer, try_cow_in_largest_gap(temp))
+        temp[1] = "1"
+        print(temp)
 
     with open("socdist1.out", "w") as output_file:
-        output_file.write(str(answer))
+        output_file.write(str(2))
