0fc838f9b4ddcc66060880fc820613c5d416d2b2
ChiSha2019 <qxia2017@gmail.com>
2021-04-14 23:41:57 -0500

    closing farm DSU

--- Silver/closing_the_farm_2016_silver/closingTheFarm_DSU.py
+++ Silver/closing_the_farm_2016_silver/closingTheFarm_DSU.py
@@ -1,20 +1,27 @@
+'''
+idea:
+1.Instead of removing the farms, we reverse the process by adding farms
+2.When adding a new farm, increase number of disjoint set by one cuz it may result in a new dijoint set.
+    check if its adjacent farm are already visited.
+        If visited, we union them and decrease the number of disjoint set by one.
+        Otherwise, do nothing
+'''
+
 class DisjSet:
     def __init__(self, n):
         # initialize sets of n items
-        self.rank = [1] * n
+        self.rank = [1] * (n+1)
         self.parent = [i for i in range(n+1)]
-        self.current_representitve = set()
+        self.parent[0] = -99
 
     # Finds set of given item x
     def find(self, x):
 
         if (self.parent[x] != x):
             self.parent[x] = self.find(self.parent[x])
-        else:
-            self.current_representitve.add(x)
         return self.parent[x]
 
-    def Union(self, x, y):
+    '''def Union(self, x, y):
 
         xset = self.find(x)
         yset = self.find(y)
@@ -22,21 +29,36 @@ class DisjSet:
         if xset == yset:
             return
 
+        self.parent[yset] = xset'''
+
+    def Union(self, x, y):
+
+        # Find current sets of x and y
+        xset = self.find(x)
+        yset = self.find(y)
+
+        # If they are already in same set
+        if xset == yset:
+            return
+
+        # Put smaller ranked item under
+        # bigger ranked item if ranks are
+        # different
         if self.rank[xset] < self.rank[yset]:
             self.parent[xset] = yset
-            self.current_representitve.remove(xset)
 
         elif self.rank[xset] > self.rank[yset]:
             self.parent[yset] = xset
-            self.current_representitve.remove(yset)
 
+        # If ranks are same, then move y under
+        # x (doesn't matter which one goes where)
+        # and increment rank of x's tree
         else:
             self.parent[yset] = xset
             self.rank[xset] = self.rank[xset] + 1
-            self.current_representitve.remove(yset)
 
 
-with open("1.in", "r") as input_file:
+with open("closing.in", "r") as input_file:
     line1 = input_file.readline().split()
     N = int(line1[0])
     M = int(line1[1])
@@ -58,7 +80,7 @@ with open("1.in", "r") as input_file:
             adjList[y] = [x]
 
     with open("closing.out", "w") as output_file:
-        close_node = [None]*N
+        close_node = [-1]*N
         for i in range(N):
             close_node[i] = int(input_file.readline())
 
@@ -66,21 +88,31 @@ with open("1.in", "r") as input_file:
         close_node.reverse()
         disjSet = DisjSet(N)
 
-        for n in adjList[close_node[0]]:
-            disjSet.Union(n,close_node[0])
+        num_of_disjoint_set =0
+        is_connected = []
+        activated = [False] * (N+1) #whether a node is currently inside of my graph
+        for i in range(N):
+            node_to_add = close_node[i]
+            num_of_disjoint_set += 1
 
-        is_connected = "NO"
-        for i in range(0,N):
-            #if this added node's parent is in existing_representitive
-            rep_set = disjSet.current_representitve
-            if disjSet.find(close_node[i]) in rep_set:
-                is_connected = "YES"
+            #add any new edges
+            activated[node_to_add] = True
+
+            for adj_n in adjList[node_to_add]:
+                #make sure this node and the adjacent node are all in the graph
+                if activated[node_to_add] and activated[adj_n] and disjSet.find(node_to_add) != disjSet.find(adj_n):
+                    disjSet.Union(node_to_add, adj_n)
+                    num_of_disjoint_set -= 1 #union two set will decrease the number of disjset
+
+            is_connected.append(num_of_disjoint_set == 1) # all connected is equivalent to num of disjoint set is 1
+
+        #this is outputed reversely
+        is_connected.reverse()
+        for bool in  is_connected:
+            if bool:
+                output_file.write("YES" + "\n")
             else:
-                is_connected = "NO"
-                #else create an independent graph with it's adj nodes
-                for n in adjList[close_node[i]]:
-                    disjSet.Union(n, close_node[0])
-            output_file.write(is_connected + "\n")
+                output_file.write("NO" + "\n")
 
 
 
